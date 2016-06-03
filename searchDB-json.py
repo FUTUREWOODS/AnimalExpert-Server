f2c918ead24e70a0a6d916d7dcc5c714042e938b
YutoAkagawa <akagawa@pcl.cs.waseda.ac.jp>
2016-06-03 19:28:25 +0900

    brush up

--- searchDB-json.py
+++ searchDB-json.py
@@ -12,9 +12,15 @@ app.config["JSON_AS_ASCII"] = False
 class juman:
 	
 	def __init__(self):
+        	self.category = "動物"
+		self.category2 = "亜種"
+		self.category3 = "１種"
+		self.category4 = "世界最大種"
+		self.country = "国"
+		self.connectionword = ""
 		cJuman.init(['-B', '-e2'])
 
-	def extractKeyWords(self, txt, category):
+	def extractKeyWords(self, txt):
 		self.keywordList = []
 		node = self.parse(txt)
 		print node
@@ -23,13 +29,34 @@ class juman:
 		wikiword = ""
 		wordsList = node.split("\n")
 		for i in range(len(wordsList)):
+			self.connectionFlag = 0
 			if "カテゴリ" in wordsList[i]:
-				self.categoryParse(wordsList[i],category, 0)
+				self.categoryParse(wordsList[i], 0)
 					
 			elif "Wikipedia上位語:" in wordsList[i]:
-				self.wikiWordsParse(wordsList[i],category)
-		if len(self.keywordList) != 0:
-			return self.keywordList[0]
+				self.wikiWordsParse(wordsList[i])
+			
+			elif "地名" in wordsList[i]:
+				self.countryParse(wordsList[i])
+				print self.connectionword			
+
+			else:
+				self.connectionword = ""
+
+		listlen = len(self.keywordList)
+		if listlen != 0:
+			while True:
+				if len(self.keywordList) > 1:
+					if self.category in self.keywordList[-1]  or self.category2 in self.keywordList[-1] or self.category3 in self.keywordList[-1] or self.category4 in self.keywordList[-1]:
+						self.keywordList.pop()
+					else:
+						return self.keywordList[-1]
+							
+				else:
+					if self.category in self.keywordList[-1]  or self.category2 in self.keywordList[-1] or self.category3 in self.keywordList[-1] or self.category4 in self.keywordList[-1]:
+						return "0"
+					else:
+						return self.keywordList[-1]
 		else:
 			return "0"
 
@@ -37,19 +64,20 @@ class juman:
 		node = cJuman.parse_opt([txt], cJuman.SKIP_NO_RESULT)
 		return node
 
-	def categoryParse(self, jumantxt, category, wikiflag):
+	def categoryParse(self, jumantxt, wikiflag):
 		categorylist = []
+
 		if "カテゴリ" in jumantxt:
 			categorylist = jumantxt.split("カテゴリ:")
-			categorylist = categorylist[1].split("\"")
-			if categorylist[0] == category:
-				categorylist = jumantxt.split(" ")
-				if not category in categorylist[0]:
-					if wikiflag == 0:
-						self.keywordList.append(categorylist[0])
-					elif wikiflag == 1:
-						self.wikiList.append(categorylist[0])
-	def wikiWordsParse(self, jumantxt,category):
+			if self.category in categorylist[1] :
+				if not "植物" in categorylist[1] :
+					categorylist = jumantxt.split(" ")
+					if not self.category in categorylist[0]:
+						if wikiflag == 0:
+							self.keywordList.append(self.connectionword + categorylist[0])
+						elif wikiflag == 1:
+							self.wikiList.append(categorylist[0])
+	def wikiWordsParse(self, jumantxt):
 		self.wikiList = []
 		categorylist = []
 		wikilist = []
@@ -57,14 +85,28 @@ class juman:
 		if "Wikipedia上位語:" in jumantxt:
 			categorylist = jumantxt.split("Wikipedia上位語:")
 			categorylist = categorylist[1].split("\"")
-			categoryList = categorylist[0].split("/")
+			categorylist = categorylist[0].split("/")
 			wikiword = categorylist[0]
-			wikiword = self.parse(wikiword)
-			self.categoryParse(wikiword,category,1)
-			if len(self.wikiList) != 0:
+			if self.category2 in categorylist[0] or self.category3 in categorylist[0] or self.category4 in categorylist[0]:
 				categorylist = jumantxt.split(" ")
-				if not category in categorylist[0]:
-					self.keywordList.append(categorylist[0])
+				self.keywordList.append(self.connectionword + categorylist[0])
+				
+			else:
+				wikiword = self.parse(wikiword)
+				self.categoryParse(wikiword,1)
+				
+				if len(self.wikiList) != 0:
+					categorylist = jumantxt.split(" ")
+					self.keywordList.append(self.connectionword + categorylist[0])
+
+	def countryParse(self, jumantxt):
+		categorylist = []
+		if "地名" in jumantxt:
+			categorylist = jumantxt.split("地名:")
+			if self.country in categorylist[1] :
+				categorylist = jumantxt.split(" ")
+				self.connectionword = categorylist[0]
+		
 
 def dict_factory(cursor, row):
     d = {}
@@ -79,8 +121,7 @@ def hello():
 @app.route('/morph/<string:txt>')
 def index(txt):
 	txt = txt.encode(TEXT_ENCODING)
-        category = "動物"
-        message = j.extractKeyWords(txt,category)
+        message = j.extractKeyWords(txt)
         return message
 
 @app.route("/<name>")
