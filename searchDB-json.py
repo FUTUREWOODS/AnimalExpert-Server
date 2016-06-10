715cb63d8210522e1e937187cfe328a2bfc0f14b
f-user <f-user@domU-9A-65-84-BF-77-7B.fvm.i1.internal>
2016-06-10 15:39:09 +0900

    extract animal name from DB

--- searchDB-json.py
+++ searchDB-json.py
@@ -12,6 +12,26 @@ def dict_factory(cursor, row):
     return d
 
 
+@app.route("/getAnimalname")
+def search():
+    conn = sqlite3.connect("./animal.db")
+    conn.row_factory = dict_factory
+    sql = "SELECT * FROM animals WHERE name LIKE ?"
+    cur = conn.cursor()
+    cur.execute(sql, [name + ';'])
+    res = cur.fetchall()
+    print res
+    """
+    # return '\n'.join(str(res))
+    dic = {}
+    for i, d in enumerate(res):
+        dic[i] = d
+
+    #return jsonify(dic)
+    """
+    return rec
+
+"""
 @app.route("/<name>")
 def search(name):
     conn = sqlite3.connect("./animal.db")
@@ -25,7 +45,7 @@ def search(name):
     for i, d in enumerate(res):
         dic[i] = d
     return jsonify(dic)
-
+"""
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False)
+   app.run(host='0.0.0.0', debug=False)
